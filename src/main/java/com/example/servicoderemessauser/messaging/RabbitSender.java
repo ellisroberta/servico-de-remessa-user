8217835dc64874ede928c0043a45511979e011a2
ellisroberta <ellisroberta@hotmail.com>
2024-05-21 15:16:56 -0300

    ajustes no RabbitSender e criacao de seus testes

--- src/main/java/com/example/servicoderemessauser/messaging/RabbitSender.java
+++ src/main/java/com/example/servicoderemessauser/messaging/RabbitSender.java
@@ -2,13 +2,24 @@ package com.example.servicoderemessauser.messaging;
 
 import com.example.servicoderemessauser.config.RabbitConfig;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 
 public class RabbitSender {
 
-    private RabbitTemplate rabbitTemplate;
+    private final RabbitTemplate rabbitTemplate;
+
+    @Value("${rabbitmq.exchange}")
+    private String exchange;
+
+    @Value("${rabbitmq.routingkey.transaction}")
+    private String transactionRoutingKey;
+
+    public RabbitSender(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
 
     public void sendTransactionMessage(TransactionMessage message) {
-        rabbitTemplate.convertAndSend(RabbitConfig.TRANSACTION_QUEUE, message);
+        rabbitTemplate.convertAndSend(exchange, transactionRoutingKey, message);
     }
     
 }
